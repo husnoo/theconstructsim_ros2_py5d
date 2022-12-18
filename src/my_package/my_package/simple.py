40ad34bc7a1083c4de2255adb590a02933d49468
Your Name <you@example.com>
2022-12-18 17:19:06 +0000

    simple node

--- src/my_package/my_package/simple.py
+++ src/my_package/my_package/simple.py
@@ -3,13 +3,33 @@ import rclpy
 from rclpy.node import Node
 
 
+class MyNode(Node):
+    def __init__(self):
+        # call super() in the constructor to initialize the Node object
+        # the parameter we pass is the node name
+        super().__init__('Byakugan')
+        # create a timer sending two parameters:
+        # - the duration between two callbacks (0.2 seconds)
+        # - the timer function (timer_callback)
+        self.create_timer(0.2, self.timer_callback)
+
+    def timer_callback(self):
+        # print a ROS2 log on the terminal with a great message!
+        self.get_logger().info("Moe yo Byakugan! Kore ga watashi no nindō yo")
+
+
 def main(args=None):
     # initialize the ROS communication
     rclpy.init(args=args)
     # print a message to the terminal
     print("Moe yo Byakugan! Kore ga watashi no nindō yo ")
     # Translated to English should be "Blaze Away, Byakugan! This is My Ninja Way!"
-    # shutdown the ROS communication
+
+    # declare the node constructor
+    node = MyNode()
+    # keeps the node alive, waits for a request to kill the node (ctrl+c)
+    rclpy.spin(node)
+    # shutdown the ROS2 communication
     rclpy.shutdown()
 
 
