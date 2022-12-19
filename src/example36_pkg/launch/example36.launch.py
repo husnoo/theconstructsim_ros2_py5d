3fbc044d9abe6af5a68b6bf176fb346f17b3e4dd
Your Name <you@example.com>
2022-12-19 17:12:52 +0000

    launch for using interface

---
+++ src/example36_pkg/launch/example36.launch.py
@@ -0,0 +1,11 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='example36_pkg',
+            executable='example36',
+            output='screen'),
+    ])
