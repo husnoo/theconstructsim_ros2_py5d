2eff1963b9fde5bced8e6e6f2fc4fc86c5d17ed6
Your Name <you@example.com>
2022-12-21 12:43:51 +0000

    printing out xyz rpy

--- src/topics_quiz/topics_quiz/topics_quiz_node.py
+++ src/topics_quiz/topics_quiz/topics_quiz_node.py
@@ -4,6 +4,33 @@ from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from rclpy.qos import ReliabilityPolicy, QoSProfile
 
+import numpy as np
+
+
+def euler_from_quaternion(quaternion):
+    """
+    Converts quaternion (w in last place) to euler roll, pitch, yaw
+    quaternion = [x, y, z, w]
+    Below should be replaced when porting for ROS2 Python tf_conversions is done.
+    """
+    x = quaternion[0]
+    y = quaternion[1]
+    z = quaternion[2]
+    w = quaternion[3]
+
+    sinr_cosp = 2 * (w * x + y * z)
+    cosr_cosp = 1 - 2 * (x * x + y * y)
+    roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+    sinp = 2 * (w * y - z * x)
+    pitch = np.arcsin(sinp)
+
+    siny_cosp = 2 * (w * z + x * y)
+    cosy_cosp = 1 - 2 * (y * y + z * z)
+    yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+    return roll, pitch, yaw
+
 
 class TopicQuiz(Node):
 
@@ -28,7 +55,13 @@ class TopicQuiz(Node):
         self.get_logger().info('Publishing: "%s"' % msg)
 
     def listener_callback(self, msg):
-        self.get_logger().info('I receive: "%s"' % str(msg))
+        #self.get_logger().info('I receive: "%s"' % str(msg))
+        pos = msg.pose.pose.position
+        orient = msg.pose.pose.orientation
+        quaternion = [orient.x, orient.y, orient.z, orient.w]
+        roll, pitch, yaw = euler_from_quaternion(quaternion)
+        self.get_logger().info(
+            'pos: {} roll: {:.2f} pitch: {:.2f} yaw: {:.2f}'.format(pos, roll, pitch, yaw))
 
 
 def main(args=None):
